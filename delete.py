ff5c12f824eabc029e4b8508cefc0f215af797e0
Prashant <weebprashant@gmail.com>
2025-08-04 22:08:23 +0545

    Add files via upload

---
+++ delete.py
@@ -0,0 +1,9 @@
+import os
+if os.path.exists("Users\\macbook\\Desktop\\Prashant\\secondfile.txt"):
+    os.remove("Users\\macbook\\Desktop\\Prashant\\secondfile.txt")
+    print("file removed successfully")
+else:
+    print("FIle not found try again!!!!")
+
+    
+ 
